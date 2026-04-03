f3a51df53fc8de78ac9dcc9a1c3561b2552a2955
Korny666 <12656521+Korny666@users.noreply.github.com>
2026-04-04 00:34:25 +0200

    Refactor to multi-system support with unsupported system error

--- flake.nix
+++ flake.nix
@@ -16,29 +16,42 @@
       nixvim,
     }:
     let
-      system = "x86_64-linux";
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
       vimconfig = import ./nix-nvim-config.nix;
-      pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages."${system}" = {
-        nix-nvim = nixvim.legacyPackages."${system}".makeNixvimWithModule {
-          inherit pkgs;
-          module = vimconfig;
-        };
-        nix-nvim-offline = pkgs.writeShellApplication {
-          name = "nvim";
-          runtimeInputs = [
-            pkgs.bubblewrap
-            self.packages."${system}".nix-nvim
-            pkgs.bash
+      nixosModules.default = { pkgs, lib, ... }:
+        let
+          sys = pkgs.stdenv.hostPlatform.system;
+        in {
+          environment.systemPackages = [
+            (self.packages.${sys}.default or (throw "nvim-nix: unsupported system ${sys}"))
           ];
-          text = ''
-            bwrap --dev-bind / / --unshare-net ${nixpkgs.lib.getExe self.packages."${system}".nix-nvim} "$@"
-          '';
         };
-        default = self.packages."${system}".nix-nvim;
-        fonts = pkgs.nerd-fonts.inconsolata-go;
-      };
+
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          nix-nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+            inherit pkgs;
+            module = vimconfig;
+          };
+          nix-nvim-offline = pkgs.writeShellApplication {
+            name = "nvim";
+            runtimeInputs = [
+              pkgs.bubblewrap
+              self.packages.${system}.nix-nvim
+              pkgs.bash
+            ];
+            text = ''
+              bwrap --dev-bind / / --unshare-net ${nixpkgs.lib.getExe self.packages.${system}.nix-nvim} "$@"
+            '';
+          };
+          default = self.packages.${system}.nix-nvim;
+          fonts = pkgs.nerd-fonts.inconsolata-go;
+        }
+      );
     };
 }
